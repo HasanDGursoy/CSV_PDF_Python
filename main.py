84df0f0d68e8977fe4da2a53c048ec9aab231658
Hasan Doğacan Gürsoy <hasan.d.gursoy@gmail.com>
2021-09-19 22:54:53 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import csv_islemleri
+
+# csv_islemleri.csv_oku()
+
+# csv_islemleri.csv_oku_noktali_virgul()
+
+# csv_islemleri.csv_oku_dilect()
+
+# csv_islemleri.csv_sniffer()
+
+# csv_islemleri.csv_yazma()
+
+csv_islemleri.csv_kopyala()
